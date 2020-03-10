aa9c5961a64a8062a0623ed732ac29f406d21a8a
Micky Chettanapanich <mickychetta@yahoo.com>
2020-03-09 23:22:34 -0700

    Added argument inputs

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define BUFFER_SIZE 8
 
-int main() {
-    printf("Hello, World!\n");
+typedef int buffer_item;
+
+buffer_item buffer[BUFFER_SIZE];
+
+int main(int argc, char **argv) {
+    int num_producer = atoi(argv[1]);
+    int num_consumer = atoi(argv[2]);
+    int num_items = atoi(argv[3]);
+    int delay = atoi(argv[4]);
+
+    printf("%d %d %d %d", num_producer, num_consumer, num_items, delay);
+
     return 0;
 }
