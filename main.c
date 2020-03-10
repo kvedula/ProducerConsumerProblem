004895f8e4a2125b7dce9279130a5d5669b77ac8
Micky Chettanapanich <mickychetta@yahoo.com>
2020-03-10 04:09:32 -0700

    Added insert and remove functions

--- main.c
+++ main.c
@@ -1,61 +1,120 @@
+//Micky Chettanapanich mchettan 90208070
+//Lily Pham
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 #define BUFFER_SIZE 8
 
-/*
- * pthread_mutex_lock
- * pthread_mutex_unlock
- * pthread_cond_wait
- * pthread_cond_signal
- * pthread_cond_broadcast
- */
+typedef int item;
+item buffer[BUFFER_SIZE];
+pthread_mutex_t mutex;
+sem_t full, empty;
+int incoming_item_index = 0;
+int outgoing_item_index = 0;
+int buffer_item_count = 0;
+int producer_count = 0;
+int consumer_count = 0;
+int item_count = 0;
 
-typedef int buffer_item;
-
-buffer_item buffer[BUFFER_SIZE];
-void lily_sucks_and_is_a_traitor();
-void micky_is_cool();
+void *producer(int delay);
+void *consumer(int delay);
+int sbuf_insert(item buffer_item);
+int sbuf_remove(item *buffer_item);
 
 int main(int argc, char **argv) {
     int num_producer = atoi(argv[1]);
     int num_consumer = atoi(argv[2]);
     int num_items = atoi(argv[3]);
     int delay = atoi(argv[4]);
+    pthread_mutex_init(&mutex, NULL);
+
+    //Initialize buffer
+    sem_init(&full, 0, 0);
+    sem_init(&empty, 0, BUFFER_SIZE);
+
+    // Initializing threads
     int i;
     pthread_t prod_thread[num_producer];
     pthread_t con_thread[num_consumer];
-    int producer[num_producer];
-    int consumer[num_consumer];
 
     for(i = 0; i < num_producer; i++)
     {
-        producer[i] = i + 1;
+        pthread_create(&prod_thread[i], NULL, producer, NULL);
     }
 
     for(i = 0; i < num_consumer; i++)
     {
-        consumer[i] = i + 1;
+        pthread_create(&con_thread[i], NULL, consumer, NULL);
     }
 
 
-
-
-
-    printf("%d %d %d %d", num_producer, num_consumer, num_items, delay);
+    //printf("%d %d %d %d", num_producer, num_consumer, num_items, delay);
 
     return 0;
 }
 
-//void lily_sucks_and_is_a_traitor(){
-//    while(1){
-//
-//    }
-//
-//}
-//
-//void micky_is_cool(){
-//    while(1){
-//
-//    }
-//}
+void *producer(int delay){
+    item buffer_item;
+
+    while(1){
+        if(delay == 0)
+            usleep(0.5);
+
+        buffer_item = 0;
+        sbuf_insert(buffer_item);
+
+        printf("producer_%s produced item %s\n", producer_count, item_count);
+    }
+}
+
+void *consumer(int delay){
+    item buffer_item;
+
+    while(1){
+        if(delay == 1)
+            usleep(0.5);
+
+        sbuf_remove(&buffer_item);
+        printf("consumer_%s consumed item %s\n", consumer_count, item_count);
+    }
+}
+
+int sbuf_insert(item buffer_item){
+    sem_wait(&empty);   // Wait for available slot
+    pthread_mutex_lock(&mutex); // Lock the buffer
+
+    // Inserting the item
+    if(buffer_item_count != BUFFER_SIZE)
+    {
+        buffer[incoming_item_index] = buffer_item;
+        incoming_item_index = (incoming_item_index + 1) % BUFFER_SIZE;
+
+        buffer_item_count++;
+    }
+
+
+    pthread_mutex_unlock(&mutex); // Unlock the buffer
+    sem_post(&full); // Announce available item
+
+}
+
+int sbuf_remove(item *buffer_item){
+    sem_wait(&full); // Wait for available item
+    pthread_mutex_lock(&mutex); // Lock the buffer
+
+    // Remove the item
+    if(buffer_item_count != 0)
+    {
+        *buffer_item = buffer[outgoing_item_index];
+        outgoing_item_index = (outgoing_item_index + 1) % BUFFER_SIZE;
+
+        buffer_item_count--;
+    }
+
+
+    pthread_mutex_unlock(&mutex); // Unlock the buffer
+    sem_post(&empty); // Announce available spot
+}
